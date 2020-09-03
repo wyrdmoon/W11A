d70498d6aff8562b32101f7ea5349b20568d9b55
wyrdmoon <criscorcoran@gmail.com>
2020-09-03 11:06:54 -0600

    just need to get the space bar event

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var hover = document.getElementById("script-2");
+hover.addEventListener("mouseover",function() {
+  hover.style.color ="blue";
+});
+
+var click = document.innerHTML= ("header");
+click.addEventListener("click", function() {
+    click.style.fontSize = "75px";
+});
+
+document.addEventListener('keyup', event => {
+    if(event.code == 'space') {
+        document.getElementById("space");
+   }
+});
+
+
